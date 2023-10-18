bc75d31da2f5e51a924b2fb7ccda7243bbc5de6d
fradelycodes <fradelydventura@gmail.com>
2023-10-18 04:24:08 -0400

    add spinner

--- app/root.tsx
+++ app/root.tsx
@@ -11,8 +11,11 @@ import {
 	ScrollRestoration,
 	useLoaderData,
 	useNavigation,
+	useSubmit,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import { useEffect } from "react";
+
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { createEmptyContact, getContacts } from "./data";
 
 import appStylesHref from "./app.css";
@@ -20,13 +23,13 @@ import appStylesHref from "./app.css";
 export const action = async () => {
 	const contact = await createEmptyContact();
 	return redirect(`/contacts/${contact.id}/edit`);
-
-	// return json({ contact });
 };
 
-export const loader = async () => {
-	const contacts = await getContacts();
-	return json({ contacts });
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const url = new URL(request.url);
+	const q = url.searchParams.get("q");
+	const contacts = await getContacts(q);
+	return json({ contacts, q });
 };
 
 export const links: LinksFunction = () => [
@@ -34,8 +37,19 @@ export const links: LinksFunction = () => [
 ];
 
 export default function App() {
-	const { contacts } = useLoaderData<typeof loader>();
+	const { contacts, q } = useLoaderData<typeof loader>();
 	const navigation = useNavigation();
+	const submit = useSubmit();
+	const searching =
+		navigation.location &&
+		new URLSearchParams(navigation.location.search).has("q");
+
+	useEffect(() => {
+		const searchField = document.getElementById("q");
+		if (searchField instanceof HTMLInputElement) {
+			searchField.value = q || "";
+		}
+	}, [q]);
 
 	return (
 		<html lang="en">
@@ -49,15 +63,31 @@ export default function App() {
 				<div id="sidebar">
 					<h1>Remix Contacts</h1>
 					<div>
-						<Form id="search-form" role="search">
+						<Form
+							id="search-form"
+							role="search"
+							onChange={(event) => {
+								const isFirstSearch = q === null;
+								submit(event.currentTarget, {
+									replace: !isFirstSearch,
+								});
+							}}
+						>
 							<input
-								id="q"
 								aria-label="Search contacts"
+								className={searching ? "loading" : ""}
+								defaultValue={q || ""}
+								id="q"
+								name="q"
 								placeholder="Search"
 								type="search"
-								name="q"
 							/>
-							<div id="search-spinner" aria-hidden hidden={true} />
+							<div
+								id="search-spinner"
+								aria-hidden
+								hidden={true}
+								hidden={!searching}
+							/>
 						</Form>
 						<Form method="post">
 							<button type="submit">New</button>
@@ -105,7 +135,9 @@ export default function App() {
 				</div>
 				<div
 					id="detail"
-					className={navigation.state === "loading" ? "loading" : ""}
+					className={
+						navigation.state === "loading" && !searching ? "loading" : ""
+					}
 				>
 					<Outlet />
 				</div>
